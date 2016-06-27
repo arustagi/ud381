ad776d02bf9c7cac9aaf8b2109b720f30ef98fe7
anrustagi <anrustagi@ebay.com>
2016-06-27 13:59:50 -0700

    lesson1 stage1 completed

--- lesson1/stage1/src/jvm/udacity/storm/ReporterExclamationTopology.java
+++ lesson1/stage1/src/jvm/udacity/storm/ReporterExclamationTopology.java
@@ -16,6 +16,9 @@ import backtype.storm.utils.Utils;
 
 import java.util.Map;
 
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisConnection;
+
 //********* TO DO 1-of-4 imported http://mvnrepository.com/artifact/com.lambdaworks/lettuce/
 
 
@@ -54,7 +57,7 @@ public class ReporterExclamationTopology {
     //********* TO DO 2-of-4
     // place holder to keep the connection to redis
 
-
+    RedisConnection<String,String> redis;
     //********* END 2-of-4
 
     @Override
@@ -68,9 +71,9 @@ public class ReporterExclamationTopology {
 
       //********* TO DO 3-of-4
       // instantiate a redis connection
-
+      RedisClient client = new RedisClient("localhost",6379);
       // initiate the actual connection
-
+      redis = client.connect();
       //********* END 3-of-4
     }
 
@@ -88,8 +91,8 @@ public class ReporterExclamationTopology {
       _collector.emit(tuple, new Values(exclamatedWord.toString()));
 
       //********* TO DO 4-of-4 Uncomment redis reporter
-      //long count = 30;
-      //redis.publish("WordCountTopology", exclamatedWord.toString() + "|" + Long.toString(count));
+      long count = 30;
+      redis.publish("WordCountTopology", exclamatedWord.toString() + "|" + Long.toString(count));
       //********* END 4-of-4
     }
 
